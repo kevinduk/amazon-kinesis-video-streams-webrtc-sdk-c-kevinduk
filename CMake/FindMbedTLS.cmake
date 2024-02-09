11246416ca0dd0768e882fc78650173a8f62fa25
kevind <kevind@Ubuntu20.04LTS.myguest.virtualbox.org>
2024-02-09 11:37:30 +0000

    Pass toolchain file to dependencies. Pass mbedtls library variables to libwebsockets dependency.

--- CMake/FindMbedTLS.cmake
+++ CMake/FindMbedTLS.cmake
@@ -1,10 +1,27 @@
+include(CMakePrintHelpers)
+message("In FindMbedTLS.cmake")
 find_path(MBEDTLS_INCLUDE_DIRS mbedtls/ssl.h)
+cmake_print_variables(MBEDTLS_INCLUDE_DIRS)
 
-find_library(MBEDTLS_LIBRARY mbedtls)
-find_library(MBEDX509_LIBRARY mbedx509)
-find_library(MBEDCRYPTO_LIBRARY mbedcrypto)
+cmake_print_variables(MBEDTLS_LIBRARY)
+cmake_print_variables(MBEDX509_LIBRARY)
+cmake_print_variables(MBEDCRYPTO_LIBRARY)
+cmake_print_variables(CMAKE_FIND_LIBRARY_PREFIXES)
+cmake_print_variables(CMAKE_FIND_LIBRARY_SUFFIXES)
+
+message("HINTS: ${OPEN_SRC_INSTALL_PREFIX}")
+
+cmake_print_variables(OPEN_SRC_INSTALL_PREFIX)
+find_library(MBEDTLS_LIBRARY NAMES mbedtls libmbedtls.a HINTS ${OPEN_SRC_INSTALL_PREFIX})
+
+cmake_print_variables(MBEDTLS_LIBRARY)
+find_library(MBEDX509_LIBRARY libmbedx509.a)
+cmake_print_variables(MBEDX509_LIBRARY)
+find_library(MBEDCRYPTO_LIBRARY libmbedcrypto.a)
+cmake_print_variables(MBEDCRYPTO_LIBRARY)
 
 set(MBEDTLS_LIBRARIES "${MBEDTLS_LIBRARY}" "${MBEDX509_LIBRARY}" "${MBEDCRYPTO_LIBRARY}")
+cmake_print_variables(MBEDTLS_LIBRARIES)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MbedTLS DEFAULT_MSG
